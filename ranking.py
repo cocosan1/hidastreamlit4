ba0d082c6c884f6209cf7f13dae585942f2d5571
hskw1 <isishskw@gmail.com>
2023-05-27 09:13:18 +0900

    28th commit

--- ranking.py
+++ ranking.py
@@ -12,13 +12,13 @@ from sklearn.preprocessing import StandardScaler
 from func_collection import Graph
 
 st.set_page_config(page_title='ranking')
-st.markdown('#### 品番別分析')
+st.markdown('## 品番別分析')
 
 @st.cache_data(ttl=datetime.timedelta(hours=1))
 def make_data_now(file):
     df_now = pd.read_excel(
     file, sheet_name='受注委託移動在庫生産照会', \
-        usecols=[2, 8, 9, 10, 15, 16, 31, 42, 50, 51]) #index　ナンバー不要　index_col=0
+        usecols=[2, 3, 8, 9, 10, 15, 16, 31, 42, 46, 50, 51]) #index　ナンバー不要　index_col=0
 
     df_now['得意先CD2'] = df_now['得意先CD'].map(lambda x:str(x)[0:5])
     df_now['商品コード2'] = df_now['商品コード'].map(lambda x: x.split()[0]) #品番
@@ -40,7 +40,7 @@ def make_data_now(file):
 def make_data_last(file):
     df_last = pd.read_excel(
     file, sheet_name='受注委託移動在庫生産照会', \
-        usecols=[2, 8, 9, 10, 15, 16, 31, 42, 50, 51])
+        usecols=[2, 3, 8, 9, 10, 15, 16, 31, 42, 46, 50, 51])
     
     df_last['得意先CD2'] = df_last['得意先CD'].map(lambda x:str(x)[0:5])
     df_last['商品コード2'] = df_last['商品コード'].map(lambda x: x.split()[0]) #品番
@@ -97,7 +97,7 @@ def calc_deviation():
     #*******************************************上昇下降アイテムの抽出
     st.markdown('### アイテム上昇・下降分析/偏差値')
 
-    cate_list = ['リビングチェア', 'ダイニングチェア', 'ダイニングテーブル']
+    cate_list = ['リビングチェア', 'ダイニングチェア', 'ダイニングテーブル', 'リビングテーブル', 'キャビネット類']
     selected_cate = st.selectbox(
         '商品分類',
         cate_list,
@@ -125,7 +125,7 @@ def calc_deviation():
 
         s_now2g = df_now2.groupby('品番')['数量'].sum()
         s_last2g = df_last2.groupby('品番')['数量'].sum()
-    
+
     else:
         df_now2 = df_now[df_now['商品分類名2']==selected_cate]
         df_last2 = df_last[df_last['商品分類名2']==selected_cate]
@@ -145,7 +145,7 @@ def calc_deviation():
 
     col1, col2 = st.columns(2)
     with col1:
-        st.write('今期 Top30')
+        st.markdown('#### 今期 Top30')
         #上位30取り出し
         s_temp_now = s_now2g.sort_values(ascending=False)
         s_temp_now = s_temp_now[0:30]
@@ -157,7 +157,7 @@ def calc_deviation():
             st.dataframe(s_now2g)
 
         #外れ値処理
-        st.write('外れ値処理')
+        st.markdown('##### 外れ値処理')
         under_now = st.number_input('下限指定', key='unn', value=0)
         upper_now = st.number_input('上限指定', key='upn', value=100)
 
@@ -167,7 +167,7 @@ def calc_deviation():
             st.write(s_now2g)
 
     with col2:
-        st.write('前期 Top30')
+        st.markdown('#### 前期 Top30')
         #上位30取り出し
         s_temp_last = s_last2g.sort_values(ascending=False)
         s_temp_last = s_temp_last[0:30]
@@ -179,7 +179,7 @@ def calc_deviation():
             st.dataframe(s_last2g) 
 
         #外れ値処理
-        st.write('外れ値処理')
+        st.markdown('##### 外れ値処理')
         under_last = st.number_input('下限指定', key='unl', value=0)
         upper_last = st.number_input('上限指定', key='upl', value=100)
 
@@ -210,6 +210,7 @@ def calc_deviation():
     df_m['差異'] = df_m['dev今期'] - df_m['dev前期']
     df_m['比率'] = df_m['dev今期'] / df_m['dev前期']
 
+    st.markdown('#### アイテム上昇・下降分析')
     #偏差値
     item_list = ['上昇アイテム', '下降アイテム']
     selected_item = st.selectbox(
@@ -257,7 +258,7 @@ def calc_deviation():
     
     #*******************************************アイテムの深堀
     #******************データの絞込み
-    st.markdown('#### 品番検索: 売れ筋組み合わせ 塗色／張地')
+    st.markdown('#### 品番検索: 売れ筋分析 塗色/張地/得意先')
 
     #品番検索
     part_word = st.text_input(
@@ -280,20 +281,78 @@ def calc_deviation():
         df_select = df_now2[df_now2['品番'] == selected_item]
 
         #******************塗色分析
-        st.write('塗色別数量')
+        st.markdown('##### 塗色別数量')
         s_color = df_select.groupby('塗色CD')['数量'].sum()
 
         s_color = s_color.sort_values(ascending=False)
         graph.make_bar(s_color, s_color.index)
 
+        #*****************張地ランキング
+        st.markdown('##### 張地別数量')
+        selected_color = st.selectbox(
+            '塗色を選択',
+            s_color.index,
+            key='sc'
+        )
+
+        df_color = df_select[df_select['塗色CD']==selected_color]
+        s_fab = df_color.groupby('張地')['数量'].sum()
+
+        s_fab = s_fab.sort_values(ascending=True)
+        graph.make_bar_h_nonline(s_fab, s_fab.index, '数量', '張地/数量順', 800)
+
         #******************塗色張地分析
         s_item = df_select.groupby('商　品　名')['数量'].sum()
 
         s_item = s_item.sort_values(ascending=True)
-        graph.make_bar_h_nonline(s_item, s_item.index, '数量', '売れ筋組み合わせ 塗色／張地', 800)
+
+        with st.expander('売れ筋組み合わせ 塗色／張地', expanded=False):
+            graph.make_bar_h_nonline(s_item, s_item.index, '数量', '売れ筋組み合わせ 塗色／張地', 800)
+
+            st.dataframe(s_item)
+        #********************購入している得意先
+        s_sum = df_select.groupby('得意先名')['数量'].sum()
+
+        s_sum = s_sum.sort_values(ascending=True)
+        graph.make_bar_h_nonline(s_sum, s_sum.index, '数量', '得意先別数量', 800)
+
+        #dataframe作成
+        sales_list = []
+        rep_list = []
+        for cust in s_sum.index:
+            #得意先の売上取得
+            df_cust = df_now[df_now['得意先名'] == cust]
+            sales_sum = df_cust['金額'].sum()
+            sales_list.append(sales_sum)
+            #得意先担当者名取得
+            rep = df_cust.iloc[0]['営業担当者名']
+            rep_list.append(rep)
+
+        df_cust = pd.DataFrame(list(zip(s_sum, sales_list, rep_list)), index=s_sum.index, \
+                        columns=['数量', '参考:売上/全商品合計', '担当者'])
+        df_cust.sort_values('数量', ascending=False, inplace=True)
 
         with st.expander('一覧', expanded=False):
-            st.dataframe(s_item)
+            st.dataframe(df_cust)
+        
+        #********************得意先別深堀
+        st.markdown('##### 得意先を選択して明細を見る')
+        selected_cust = st.selectbox(
+            '得意先を選択',
+            df_cust.index,
+            key='scust')
+        
+        df_cust2 = df_select[df_select['得意先名']==selected_cust]
+        s_cust = df_cust2.groupby('商　品　名')['数量'].sum()
+
+        s_cust = s_cust.sort_values(ascending=True)
+
+        with st.expander('選択した得意先の明細を見る: 組み合わせ 塗色／張地', expanded=False):
+            graph.make_bar_h_nonline(s_cust, s_cust.index, '数量', '組み合わせ 塗色／張地', 500)
+
+        
+
+        
         
         #アソシエーション分析へのリンク
         st.markdown('#### 同時に買われているアイテムを見る')
